982a7875588aabca751d2d386d84fd69a5f5ffa1
rreda11 <141592396+rreda11@users.noreply.github.com>
2025-07-28 23:33:05 +0300

    Add files via upload

---
+++ alx_backend_graphql/schema.py
@@ -0,0 +1,19 @@
+import graphene
+import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
+
+schema = graphene.Schema(query=Query)
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
